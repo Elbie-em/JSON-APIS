b7eb3e0d5e0f1b5a39bbfc105a7855ec6b5ef5bd
Elbie-em <emoonga1993@gmail.com>
2020-08-07 22:54:11 +0200

    Get JSON api data from server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
+
+// Request URL from github
+let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+
+let request = new XMLHttpRequest();
+
+request.open('GET',requestURL);
+
+//setting the responseType to JSON, so that XHR knows that the server will be returning JSON, and that this should be converted behind the scenes into a JavaScript object
+request.responseType = 'json';
+
+//send the request with the send() method
+request.send();
+
+request.onload = () => {
+    const superheroes = request.response;
+}

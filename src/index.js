8a096e035d29a35853cec998b84342d8a4a6ed82
Elbie-em <emoonga1993@gmail.com>
2020-08-10 12:19:48 +0200

    Create show heroes , Call show heroes method

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ request.send();
 request.onload = () => {
     const superheroes = request.response;
     populateHeader(superheroes);
+    showHeroes(superHeroes);
 }
 
 const populateHeader = (jsonObject) => {
@@ -25,4 +26,37 @@ const populateHeader = (jsonObject) => {
 
     const elemPg = document.createElement('p');
     elemPg.textContent = `Hometown: ${jsonObject['homeTown']} // Formed: ${jsonObject['formed']}`;
+}
+
+const showHeroes  = (jsonObject) => {
+    const heroes = jsonObject['members'];
+      
+  for (let i = 0; i < heroes.length; i++) {
+    const myArticle = document.createElement('article');
+    const myH2 = document.createElement('h2');
+    const myPara1 = document.createElement('p');
+    const myPara2 = document.createElement('p');
+    const myPara3 = document.createElement('p');
+    const myList = document.createElement('ul');
+
+    myH2.textContent = heroes[i].name;
+    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+    myPara2.textContent = 'Age: ' + heroes[i].age;
+    myPara3.textContent = 'Superpowers:';
+        
+    const superPowers = heroes[i].powers;
+    for (let j = 0; j < superPowers.length; j++) {
+      const listItem = document.createElement('li');
+      listItem.textContent = superPowers[j];
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
 }

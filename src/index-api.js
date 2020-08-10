3349ac0cc1958dc44b33a6bdef56d9d9d20ec620
Elbie-em <emoonga1993@gmail.com>
2020-08-10 20:20:49 +0200

    Add simple validation

--- src/index-api.js
+++ src/index-api.js
@@ -1,24 +1,33 @@
-const container = document.getElementById('container');
+const container = document.getElementById('img-container');
 
 const searchBox = document.getElementById('search-val');
 const searchBtn = document.getElementById('search-btn');
 
 searchBtn.onclick = () => {
-    
+    container.innerHTML = '';
     const img = document.createElement('img');
     searchVal = searchBox.value;
 
-    let webUrl = `https://api.giphy.com/v1/gifs/translate?api_key=m4l4htXckYyr6G8S9cbCNCmAczFhyTKx&s=${searchVal}`;
+    if (searchVal === '') {
+        const error = document.createElement('small');
+        error.innerHTML = `We cannot find what you are looking for!!! <br> Please try again`;
+        error.className = 'text-danger font-weight-lighter';
+        container.appendChild(error);
+    } else {
+        let webUrl = `https://api.giphy.com/v1/gifs/translate?api_key=m4l4htXckYyr6G8S9cbCNCmAczFhyTKx&s=${searchVal}`;
 
-    fetch(webUrl, { mode: 'cors' })
-        .then(response => {
-            return response.json();
-        }).then(response => {
-            img.src = response.data.images.original.url;
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    
-    container.appendChild(img)
+        fetch(webUrl, { mode: 'cors' })
+            .then(response => {
+                return response.json();
+            }).then(response => {
+                img.src = response.data.images.original.url;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+        container.appendChild(img)
+    }
+
+
 }
